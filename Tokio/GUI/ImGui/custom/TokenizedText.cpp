2b92fa064741e40cc0057a4f26898c19bfe255df
thedemons <thedemonsenc@gmail.com>
2022-10-04 13:15:00 +0700

    Remove old custom text rendering technique, replace it with a more flexible ImGui::TokenizedText

---
+++ Tokio/GUI/ImGui/custom/TokenizedText.cpp
@@ -0,0 +1,270 @@
+#pragma once
+#include "stdafx.h"
+#include "TokenizedText.h"
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "GUI/ImGui/imgui_internal.h"
+
+namespace ImGui
+{
+
+
+TokenizedText::TokenizedText(const char* text, ImU32 color)
+{
+    m_tokens.emplace_back(text, color);
+}
+
+TokenizedText::TokenizedText(const std::string& string, ImU32 color)
+{
+    m_tokens.emplace_back(string, color);
+}
+
+
+TokenizedText::TokenizedText(ImU32 color, const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    ImGuiContext& g = *GImGui;
+    ImFormatStringV(g.TempBuffer.Data, g.TempBuffer.Size, fmt, args);
+
+    va_end(args);
+
+    m_tokens.emplace_back(g.TempBuffer.Data, color);
+}
+
+TokenizedText::TokenizedText(const char* text, ImVec4 color)
+{
+    m_tokens.emplace_back(text, ImGui::ColorConvertFloat4ToU32(color));
+}
+
+TokenizedText::TokenizedText(const std::string& string, ImVec4 color)
+{
+    m_tokens.emplace_back(string, ImGui::ColorConvertFloat4ToU32(color));
+}
+
+TokenizedText::TokenizedText(ImVec4 color, const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    ImGuiContext& g = *GImGui;
+    ImFormatStringV(g.TempBuffer.Data, g.TempBuffer.Size, fmt, args);
+
+    va_end(args);
+
+    m_tokens.emplace_back(g.TempBuffer.Data, ImGui::ColorConvertFloat4ToU32(color));
+}
+
+TokenizedText::TokenizedText(size_t reserved)
+{
+    if (reserved > 0) m_tokens.reserve(reserved);
+}
+
+// prefer to use ImU32 for performance
+void TokenizedText::push_back(const char* text, ImVec4 color)
+{
+    m_tokens.emplace_back(text, ImGui::ColorConvertFloat4ToU32(color));
+}
+
+void TokenizedText::push_back(const std::string& string, ImVec4 color)
+{
+    m_tokens.emplace_back(string, ImGui::ColorConvertFloat4ToU32(color));
+}
+
+// prefer to use ImU32 for performance
+void TokenizedText::push_back(ImVec4 color, const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    ImGuiContext& g = *GImGui;
+    ImFormatStringV(g.TempBuffer.Data, g.TempBuffer.Size, fmt, args);
+
+    va_end(args);
+
+    m_tokens.emplace_back(g.TempBuffer.Data, ImGui::ColorConvertFloat4ToU32(color));
+}
+
+void TokenizedText::push_back(const char* text, ImU32 color)
+{
+    m_tokens.emplace_back(text, color);
+}
+
+void TokenizedText::push_back(const std::string& string, ImU32 color)
+{
+    m_tokens.emplace_back(string, color);
+}
+
+void TokenizedText::push_back(ImU32 color, const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    ImGuiContext& g = *GImGui;
+    ImFormatStringV(g.TempBuffer.Data, g.TempBuffer.Size, fmt, args);
+
+    va_end(args);
+
+    m_tokens.emplace_back(g.TempBuffer.Data, color);
+}
+
+// render in the window, same as ImGui::Text()
+void TokenizedText::Render(ImFont* font)
+{
+    if (m_tokens.size() == 0) return;
+
+    ImGuiWindow* window = GetCurrentWindow();
+    if (window->SkipItems)  return;
+
+    ImGuiContext& g = *GImGui;
+    if (font == nullptr) font = g.Font;
+
+    // we don't call ImGui::SameLine() for the last token
+    for (auto iter = m_tokens.begin(); iter != m_tokens.end() - 1; iter++)
+    {
+        RenderToken(g, window, font, font->FontSize, *iter);
+        SameLine(0, 0);
+    }
+
+    // render the last token
+    RenderToken(g, window, font, font->FontSize, m_tokens.back());
+}
+
+// render in the drawlist, same as ImDrawList->AddText()
+void TokenizedText::Render(ImDrawList* drawList, ImVec2 pos, ImFont* font)
+{
+    if (m_tokens.size() == 0) return;
+
+    ImGuiContext& g = *GImGui;
+    if (font == nullptr) font = g.Font;
+
+    // might implement it in the future
+    float wrap_width = 0.f;
+    ImVec2 currentPosition = pos;
+
+    for (Token& token : m_tokens)
+    {
+        const char* text_begin = token.text.c_str();
+        const char* text_end = text_begin + token.text.size();
+
+        const ImVec2 text_size = CalcTextSize(text_begin, text_end, false, wrap_width);
+
+        //font->RenderText()
+        drawList->AddText(font, font->FontSize, currentPosition, token.color, text_begin, text_end);
+        currentPosition.x += text_size.x;
+    }
+}
+
+// MODIFIED ImGui::TextEx
+void TokenizedText::RenderToken(ImGuiContext& g, ImGuiWindow* window, ImFont* font, float fontSize, const Token& token)
+{
+    const ImVec2 text_pos(window->DC.CursorPos.x, window->DC.CursorPos.y + window->DC.CurrLineTextBaseOffset);
+    const float wrap_pos_x = window->DC.TextWrapPos;
+    const bool wrap_enabled = (wrap_pos_x >= 0.0f);
+
+    const char* text_begin = token.text.c_str();
+    const char* text_end = text_begin + token.text.size(); // not including NULL byte at the end
+
+    if (token.text.size() <= 2000 || wrap_enabled)
+    {
+        // Common case
+        const float wrap_width = wrap_enabled ? CalcWrapWidthForPos(window->DC.CursorPos, wrap_pos_x) : 0.0f;
+        const ImVec2 text_size = CalcTextSize(text_begin, text_end, false, wrap_width);
+
+        ImRect bb(text_pos, text_pos + text_size);
+        ItemSize(text_size, 0.0f);
+        if (!ItemAdd(bb, 0))
+            return;
+
+        // Render (we don't hide text after ## in this end-user function)
+        // MODIFIED: shortcut for RenderTextWrappedCustom
+        if (text_begin != text_end)
+        {
+            window->DrawList->AddText(g.Font, g.FontSize, bb.Min, token.color, text_begin, text_end, wrap_width);
+        }
+    }
+    else
+    {
+        // Long text!
+        // Perform manual coarse clipping to optimize for long multi-line text
+        // - From this point we will only compute the width of lines that are visible. Optimization only available when word-wrapping is disabled.
+        // - We also don't vertically center the text within the line full height, which is unlikely to matter because we are likely the biggest and only item on the line.
+        // - We use memchr(), pay attention that well optimized versions of those str/mem functions are much faster than a casually written loop.
+        const char* line = text_begin;
+        const float line_height = g.FontSize; // MODIFIED: GetTextLineHeight() removed
+        ImVec2 text_size(0, 0);
+
+        // Lines to skip (can't skip when logging text)
+        ImVec2 pos = text_pos;
+        if (!g.LogEnabled)
+        {
+            int lines_skippable = (int)((window->ClipRect.Min.y - text_pos.y) / line_height);
+            if (lines_skippable > 0)
+            {
+                int lines_skipped = 0;
+                while (line < text_end && lines_skipped < lines_skippable)
+                {
+                    const char* line_end = static_cast<const char*>(memchr(line, '\n', text_end - line));
+                    if (!line_end)
+                        line_end = text_end;
+                    //if ((flags & ImGuiTextFlags_NoWidthForLargeClippedText) == 0)
+                    //    text_size.x = ImMax(text_size.x, CalcTextSize(line, line_end, false).x);
+                    line = line_end + 1;
+                    lines_skipped++;
+                }
+                pos.y += lines_skipped * line_height;
+            }
+        }
+
+        // Lines to render
+        if (line < text_end)
+        {
+            ImRect line_rect(pos, pos + ImVec2(FLT_MAX, line_height));
+            while (line < text_end)
+            {
+
+                if (!line_rect.Overlaps(window->ClipRect) && !g.LogEnabled) // MODIFIED: IsClippedEx removed
+                    break;
+
+                //if (IsClippedEx(line_rect, 0))
+                //    break;
+
+                const char* line_end = (const char*)memchr(line, '\n', text_end - line);
+                if (!line_end)
+                    line_end = text_end;
+                text_size.x = ImMax(text_size.x, CalcTextSize(line, line_end).x);
+
+                // MODIFIED: shortcut for RenderText()
+                window->DrawList->AddText(g.Font, g.FontSize, pos, GetColorU32(ImGuiCol_Text), line, line_end);
+
+                line = line_end + 1;
+                line_rect.Min.y += line_height;
+                line_rect.Max.y += line_height;
+                pos.y += line_height;
+            }
+
+            // Count remaining lines
+            int lines_skipped = 0;
+            while (line < text_end)
+            {
+                const char* line_end = (const char*)memchr(line, '\n', text_end - line);
+                if (!line_end)
+                    line_end = text_end;
+                //if ((flags & ImGuiTextFlags_NoWidthForLargeClippedText) == 0)
+                //    text_size.x = ImMax(text_size.x, CalcTextSize(line, line_end).x);
+                line = line_end + 1;
+                lines_skipped++;
+            }
+            pos.y += lines_skipped * line_height;
+        }
+        text_size.y = (pos - text_pos).y;
+
+        ImRect bb(text_pos, text_pos + text_size);
+        ItemSize(text_size, 0.0f);
+        ItemAdd(bb, 0);
+    }
+}
+
+}
+
---
+++ Tokio/GUI/ImGui/custom/TokenizedText.h
@@ -0,0 +1,164 @@
+#pragma once
+#ifndef TOKIO_IMGUI_CUSTOM_TOKENIZED_TEXT_H
+#define TOKIO_IMGUI_CUSTOM_TOKENIZED_TEXT_H
+
+#ifndef IMGUI_API
+#include "GUI/ImGui/imgui.h"
+#endif // ! IMGUI_API
+
+#include <string>
+#include <vector>
+
+struct IMGUI_API ImGuiWindow;
+struct ImGuiContext;
+
+namespace ImGui
+{
+class TokenizedText
+{
+private:
+    struct Token
+    {
+        ImU32 color;
+        std::string text;
+        Token(const char* text, ImU32 color) : text(text), color(color) {}
+        Token(const std::string& string, ImU32 color) : text(string), color(color) {}
+
+        template <size_t Size>
+        Token(const char(&text)[Size], ImU32 color) : text(text, Size), color(color) {}
+    };
+
+    std::vector<Token> m_tokens;
+
+    // MODIFIED ImGui::TextEx
+    void RenderToken(ImGuiContext& g, ImGuiWindow* window, ImFont* font, float fontSize, const Token& token);
+
+public:
+    // reserved size for the vector
+    TokenizedText(size_t reserved = 0);
+
+    TokenizedText(const char* text, ImU32 color);
+    TokenizedText(const std::string& string, ImU32 color);
+    TokenizedText(ImU32 color, const char* fmt, ...);
+
+    TokenizedText(const char* text, ImVec4 color);
+    TokenizedText(const std::string& string, ImVec4 color);
+    TokenizedText(ImVec4 color, const char* fmt, ...);
+
+    // prefer to use ImU32 for performance
+    void push_back(const char* text, ImU32 color);
+    void push_back(const std::string& string, ImU32 color);
+    void push_back(ImU32 color, const char* fmt, ...);
+
+    // prefer to use ImU32 for performance
+    void push_back(const char* text, ImVec4 color);
+    void push_back(const std::string& string, ImVec4 color);
+    void push_back(ImVec4 color, const char* fmt, ...);
+
+    // render in the window, same as ImGui::Text()
+    void Render(ImFont* font = nullptr);
+
+    // render in the drawlist, same as ImDrawList->AddText()
+    void Render(ImDrawList* drawList, ImVec2 pos, ImFont* font = nullptr);
+
+    template <size_t Size>
+    TokenizedText(const char(&text)[Size], ImU32 color)
+    {
+        m_tokens.emplace_back(text, color);
+    }
+
+    template <size_t Size>
+    _CONSTEXPR20 void push_back(const char(&text)[Size], ImU32 color)
+    {
+        m_tokens.emplace_back(text, color);
+    }
+
+    _NODISCARD _CONSTEXPR20 size_t size() const noexcept
+    {
+        return m_tokens.size();
+    }
+
+    _CONSTEXPR20 TokenizedText& operator+=(const TokenizedText& b) 
+    {
+        if (b.size() > 0)
+        {
+            m_tokens.insert(m_tokens.end(), b.m_tokens.begin(), b.m_tokens.end());
+        }
+
+        return *this;
+    }
+
+    _CONSTEXPR20 TokenizedText& operator+=(const std::string& b) 
+    {
+        push_back(b, ImGui::GetColorU32(ImGuiCol_Text));
+        return *this;
+    }
+
+    template <size_t Size>
+    _CONSTEXPR20 TokenizedText& operator+=(const char(&b)[Size])
+    {
+        push_back(b, ImGui::GetColorU32(ImGuiCol_Text));
+        return *this;
+    }
+
+    _CONSTEXPR20 TokenizedText& operator+=(const char* b) 
+    {
+        push_back(b, ImGui::GetColorU32(ImGuiCol_Text));
+        return *this;
+    }
+
+
+    _CONSTEXPR20 TokenizedText operator+(const TokenizedText& b)
+    {
+        TokenizedText c = *this;
+
+        if (b.size() > 0)
+        {
+            c.m_tokens.insert(c.m_tokens.end(), b.m_tokens.begin(), b.m_tokens.end());
+        }
+
+        return c;
+    }
+
+    _CONSTEXPR20 TokenizedText operator+(const std::string& b)
+    {
+        TokenizedText c = *this;
+        c.push_back(b, ImGui::GetColorU32(ImGuiCol_Text));
+
+        return c;
+    }
+
+    template <size_t Size>
+    _CONSTEXPR20 TokenizedText operator+(const char(&b)[Size])
+    {
+        TokenizedText c = *this;
+        c.push_back(b, ImGui::GetColorU32(ImGuiCol_Text));
+
+        return c;
+    }
+
+    _CONSTEXPR20 TokenizedText operator+(const char* b)
+    {
+        TokenizedText c = *this;
+        c.push_back(b, ImGui::GetColorU32(ImGuiCol_Text));
+
+        return c;
+    }
+
+    // insert a space if the last token doesn't end with a space
+    _CONSTEXPR20 void append_space(const TokenizedText& b)
+    {
+        if (b.size() == 0) return;
+
+        if (size() > 0)
+        {
+            auto& lastToken = m_tokens.back();
+            if (lastToken.text.back() != ' ') lastToken.text += ' ';
+        }
+
+        m_tokens.insert(m_tokens.end(), b.m_tokens.begin(), b.m_tokens.end());
+    }
+};
+}
+
+#endif // !TOKIO_IMGUI_CUSTOM_TOKENIZED_TEXT_H
