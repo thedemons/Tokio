4a0e0c212b06be1151c0958d4301b48867f214bf
thedemons <thedemonsenc@gmail.com>
2022-10-04 13:16:11 +0700

    Minor change

--- Tokio/imgui.hpp
+++ Tokio/imgui.hpp
@@ -1,13 +1,15 @@
 #pragma once
 #ifndef IMGUI_API
 
+// for ImVec math operations
 #define IMGUI_DEFINE_MATH_OPERATORS
+
+// disable ImGui's debugging tools
 #define IMGUI_DISABLE_DEBUG_TOOLS
 
 #include "GUI/ImGui/imgui.h"
 #include "GUI/ImGui/imgui_internal.h"
 #include "GUI/ImGui/Impl/imgui_impl_dx11.h"
 #include "GUI/ImGui/Impl/imgui_impl_win32.h"
-
 
 #endif // IMGUI_API
