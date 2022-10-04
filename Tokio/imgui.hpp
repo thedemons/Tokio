562169784421c8bd339416910bb3837a7349b514
thedemons <thedemonsenc@gmail.com>
2022-10-04 22:33:22 +0700

    Add warning suppress (not necessary)

--- Tokio/imgui.hpp
+++ Tokio/imgui.hpp
@@ -7,9 +7,12 @@
 // disable ImGui's debugging tools
 #define IMGUI_DISABLE_DEBUG_TOOLS
 
+#pragma warning (push, 4)
+#pragma warning( suppress : 4005)
 #include "GUI/ImGui/imgui.h"
 #include "GUI/ImGui/imgui_internal.h"
 #include "GUI/ImGui/Impl/imgui_impl_dx11.h"
 #include "GUI/ImGui/Impl/imgui_impl_win32.h"
+#pragma warning (pop)
 
 #endif // IMGUI_API
