03449aebb302a89bcf270abd81fb193e136efaa4
thedemons <thedemonsenc@gmail.com>
2022-10-06 03:49:22 +0700

    Redefine some stuffs

--- Tokio/Engine/EngineAnalyzerDef.h
+++ Tokio/Engine/EngineAnalyzerDef.h
@@ -12,38 +12,10 @@
 
 // ================================== ANALYZER DEFINITIONS ==================================
 
+struct AnalyzedInstruction;
 struct AnalyzedData;
+struct SubroutineBlock;
 struct SubroutineInfo;
-
-// block of codes belongs to the subroutine
-struct SubroutineBlock
-{
-	// index of the next block in SubroutineInfo
-	size_t iNextBlock = UPTR_UNDEFINED;
-
-	// index of the next conditional block in SubroutineInfo
-	size_t iConditionalBlock = UPTR_UNDEFINED;
-
-	// the start index of its instructions in the root
-	// we don't want to each individual block to store its instructions
-	// while we could query from the root AnalyzeData
-	size_t insStartIndex = UPTR_UNDEFINED;
-
-	// the end index of its instructions in the root
-	size_t insEndIndex = UPTR_UNDEFINED;
-
-	// reference to the root AnalyzeData of this block
-	const AnalyzedData& root;
-
-	// reference to the parent SubroutineInfo of this block
-	const SubroutineInfo& subroutine;
-
-	SubroutineBlock(const AnalyzedData& root, const SubroutineInfo& subroutine) :
-		root(root), subroutine(subroutine)
-	{
-	}
-};
-
 
 struct SubroutineInfo
 {
@@ -105,6 +77,207 @@ struct SubroutineInfo
 		return blocks.emplace_back(root, *this);
 	}
 
+	// get a block at x index
+	_NODISCARD _CONSTEXPR20  const SubroutineBlock& GetBlock(size_t index) const
+	{
+		return blocks[index];
+	}
+};
+
+
+// block of codes belongs to the subroutine
+struct SubroutineBlock
+{
+	// the reason we don't use pointers here is because when analyzing
+	// we didn't know the maxium size of blocks a subroutine can have
+	// and therefore couldn't use resize() and must use emplace_back()
+	// that reallocate the buffer everytime making the pointer obsolete
+	// use the GetNextBlock() and GetConditionalBlock() methods for easier access
+	
+	// index of the next block in SubroutineInfo
+	size_t nextBlockIndex = UPTR_UNDEFINED;
+
+	// index of the next conditional block in SubroutineInfo
+	size_t nextCondBlockIndex = UPTR_UNDEFINED;
+
+	// index of the previous block in SubroutineInfo
+	size_t prevBlockIndex = UPTR_UNDEFINED;
+
+	// index of the previous conditional block in SubroutineInfo
+	size_t prevCondBlockIndex = UPTR_UNDEFINED;
+
+	// the start index of its instructions in the root
+	// we don't want each individual block to store its instructions
+	// while we could query from the root AnalyzedData
+	size_t instructionIndex = UPTR_UNDEFINED;
+
+	// number of instructions
+	size_t instructionCount = 0;
+
+	// reference to the root AnalyzeData of this block
+	const AnalyzedData& root;
+
+	// reference to the parent SubroutineInfo of this block
+	const SubroutineInfo& subroutine;
+
+	SubroutineBlock(const AnalyzedData& root, const SubroutineInfo& subroutine) :
+		root(root), subroutine(subroutine)
+	{
+	}
+
+	// Get the next block
+	_NODISCARD _CONSTEXPR20 const SubroutineBlock* GetNextBlock() const
+	{
+		return (nextBlockIndex >= 0 && nextBlockIndex < subroutine.blocks.size()) ? &subroutine.blocks[nextBlockIndex] : nullptr;
+	}
+
+	// Get the next conditional block
+	_NODISCARD _CONSTEXPR20 const SubroutineBlock* GetNextCondBlock() const
+	{
+		return (nextCondBlockIndex >= 0 && nextCondBlockIndex < subroutine.blocks.size()) ? &subroutine.blocks[nextCondBlockIndex] : nullptr;
+	}
+
+	// Get the previous block
+	_NODISCARD _CONSTEXPR20 const SubroutineBlock* GetPrevBlock() const
+	{
+		return (prevBlockIndex >= 0 && prevBlockIndex < subroutine.blocks.size()) ? &subroutine.blocks[prevBlockIndex] : nullptr;
+	}
+
+	// Get the previous conditional block
+	_NODISCARD _CONSTEXPR20 const SubroutineBlock* GetPrevCondBlock() const
+	{
+		return (prevCondBlockIndex >= 0 && prevCondBlockIndex < subroutine.blocks.size()) ? &subroutine.blocks[prevCondBlockIndex] : nullptr;
+	}
+
+};
+
+
+
+// Output of the BaseAnalyzer
+//
+//  ## WARNING-PERFORMANCE WARNING-FIXME: (THIS IS NOT ENABLED FOR NOW)
+//	We have a seperated vector of instructions and its extra data because
+//	we used std::move to move the result returned from BaseDisassembler::Disasm
+//	into this struct, while we could copy its content into AnalyzedInstruction
+//	We basically sacrifice code-readability for a little boost of performance.
+struct AnalyzedData
+{
+	// a list of subroutines
+	std::vector<SubroutineInfo> subroutines;
+
+	// the analyzed instructions
+	std::vector<AnalyzedInstruction> instructions;
+
+	// the analyzed instructions
+	//std::vector<AnalyzedInstruction> analyzedInstructions;
+
+	_NODISCARD _CONSTEXPR20 SubroutineInfo& AddSubroutine()
+	{
+		return subroutines.emplace_back(*this);
+	}
+
+	_NODISCARD _CONSTEXPR20 AnalyzedInstruction& AddInstruction()
+	{
+		return instructions.emplace_back(*this);
+	}
+
+	AnalyzedData(){}
+
+	AnalyzedData(const AnalyzedData& o)
+	{
+		printf("i was copied %lld\n", o.instructions.size());
+
+	}
+
+	// return an instruction at x index
+	_NODISCARD _CONSTEXPR20 const AnalyzedInstruction& GetInstruction(size_t index) const
+	{
+		return instructions[index];
+	}
+
+	// return a subroutine at x index
+	_NODISCARD _CONSTEXPR20 const SubroutineInfo& GetSubroutine(size_t index) const
+	{
+		return subroutines[index];
+	}
+
+	//void copy(const AnalyzedData& o)
+	//{
+	//	// FIXME-PERFORMANCE
+	//	// We could do this ONLY IF IT IS NOT PERFORMANCE-CRITICAL
+	//	// We must copy and reconstruct one-by-one because it has a reference to AnalyzedData and other stuffs
+	//	// 
+	//	// i know i know.. it's ugly
+
+	//	this->instructions.clear();
+	//	this->subroutines.clear();
+	//	this->instructions.reserve(o.instructions.size());
+	//	this->subroutines.reserve(o.subroutines.size());
+
+	//	for (const AnalyzedInstruction& ins : o.instructions)
+	//	{
+	//		auto& copiedIns = this->AddInstruction();
+	//		copiedIns.address = ins.address;
+	//		copiedIns.length = ins.length;
+	//		copiedIns.bufferOffset = ins.bufferOffset;
+	//		copiedIns.referencedAddress = ins.referencedAddress;
+	//		copiedIns.referencedIndex = ins.referencedIndex;
+	//		copiedIns.mnemonic = ins.mnemonic;
+	//		copiedIns.operands = ins.operands;
+	//		copiedIns.blockIndex = ins.blockIndex;
+	//		copiedIns.referencedValue = ins.referencedValue;
+	//		copiedIns.isRefPointer = ins.isRefPointer;
+	//		copiedIns.isNotReadable = ins.isNotReadable;
+	//		copiedIns.isAtBaseModule = ins.isAtBaseModule;
+	//		copiedIns.isAtSubroutineStart = ins.isAtSubroutineStart;
+	//		copiedIns.isAtSubroutineEnd = ins.isAtSubroutineEnd;
+	//		copiedIns.fmtAddress = ins.fmtAddress;
+	//		copiedIns.fmtComment = ins.fmtComment;
+	//		copiedIns.fmtMnemonic = ins.fmtMnemonic;
+	//		copiedIns.fmtOperand = ins.fmtOperand;
+
+
+	//		// calculate the address before-hand here, no issue...
+	//		// don't worry about std::vector reallocate since we reserved enough size
+	//		for (auto& referer : ins.referers)
+	//		{
+	//			size_t index = referer - &o.instructions[0];
+	//			copiedIns.referers.emplace_back(&instructions[0] + index);
+	//		}
+
+	//		size_t index = ins.referencedInstruction - &o.instructions[0];
+	//		copiedIns.referencedInstruction = &instructions[0] + index;
+	//	}
+
+	//	this->subroutines.reserve(o.subroutines.size());
+
+	//	for (const SubroutineInfo& sub : o.subroutines)
+	//	{
+	//		auto& copiedSub = this->AddSubroutine();
+	//		copiedSub.start = sub.start;
+	//		copiedSub.end = sub.end;
+	//		copiedSub.blocks.reserve(sub.blocks.size());
+
+	//		for (const SubroutineBlock& block : sub.blocks)
+	//		{
+	//			auto& copiedBlock = copiedSub.AddBlock();
+	//			copiedBlock.nextBlockIndex = block.nextBlockIndex;
+	//			copiedBlock.nextCondBlockIndex = block.nextCondBlockIndex;
+	//			copiedBlock.insStartIndex = block.insStartIndex;
+	//			copiedBlock.insEndIndex = block.insEndIndex;
+	//		}
+	//	}
+
+	//	for (size_t n = 0; n < o.instructions.size(); n++)
+	//	{
+	//		assert(instructions[n].address == o.instructions[n].address);
+	//	}
+	//	for (size_t n = 0; n < o.subroutines.size(); n++)
+	//	{
+	//		assert(subroutines[n].start == o.subroutines[n].start);
+	//		assert(subroutines[n].end == o.subroutines[n].end);
+	//	}
+	//}
 };
 
 
@@ -147,8 +320,11 @@ struct AnalyzedInstruction
 	// a pointer to the referenced instruction
 	AnalyzedInstruction* referencedInstruction = nullptr;
 
-	// if this instruction is a start of a block, this will be the index of the block in the SubroutineInfo
-	size_t iBlock = UPTR_UNDEFINED;
+	// the index of the block if it belongs to any blocks
+	size_t blockIndex = UPTR_UNDEFINED;
+
+	// the index of the subroutine if it belongs to any subroutines
+	size_t iSubroutine = UPTR_UNDEFINED;
 
 	// the value of referencedAddress if is is a pointer (isRefPointer)
 	POINTER referencedValue = 0ull;
@@ -193,37 +369,12 @@ struct AnalyzedInstruction
 	{
 		return address == v;
 	};
-};
 
-// Output of the BaseAnalyzer
-//
-//  ## WARNING-PERFORMANCE WARNING-FIXME: (THIS IS NOT ENABLED FOR NOW)
-//	We have a seperated vector of instructions and its extra data because
-//	we used std::move to move the result returned from BaseDisassembler::Disasm
-//	into this struct, while we could copy its content into AnalyzedInstruction
-//	We basically sacrifice code-readability for a little boost of performance.
-struct AnalyzedData
-{
-	// a list of subroutines
-	std::vector<SubroutineInfo> subroutines;
 
-	// the analyzed instructions
-	std::vector<AnalyzedInstruction> instructions;
-
-	// the analyzed instructions
-	//std::vector<AnalyzedInstruction> analyzedInstructions;
-
-	_NODISCARD _CONSTEXPR20 SubroutineInfo& AddSubroutine()
+	_NODISCARD _CONSTEXPR20 const SubroutineInfo* GetSubroutine() const
 	{
-		return subroutines.emplace_back(*this);
-	}
-
-	_NODISCARD _CONSTEXPR20 AnalyzedInstruction& AddInstruction()
-	{
-		return instructions.emplace_back(*this);
+		return (iSubroutine >= 0 && iSubroutine < root.subroutines.size()) ? &root.subroutines[iSubroutine] : nullptr;
 	}
 };
-
-
 
 #endif // !TOKIO_ENGINE_ENGINE_ANALYZER_DEF_H
